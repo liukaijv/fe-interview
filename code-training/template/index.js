4552a62539b0075acb8876fb2f6f9222edecf41f
abc <liukaijv@foxmail.com>
2021-05-25 17:10:38 +0800

    template

--- code-training/template/index.js
+++ code-training/template/index.js
@@ -1,20 +1,26 @@
 export function simpleTpl(tpl, data) {
-    let reg = /<%\=([^=%>]+)?%>/g;
+    let reg = /<%([^%>]+)?%>/g;
     let code = `var r=[];\n`;
     let match;
     let cursor = 0;
 
     let add = (line, js) => {
         if (js) {
-            code += `r.push(${line});\n`;
+            if (/(if|for|else|switch|case|break|\{|\})(.*)?/g.test(line)) {
+                code += `${line}\n`;
+            } else if (/^\=(.*)?/g.test(line)) {
+                code += `r.push(${line.replace('=', '')});\n`;
+            }
         } else {
-            code += `r.push("${line.replace(/"/, '\\\\"')}");\n`;
+            if (line !== '') {
+                code += `r.push("${line.replace(/"/g, '\\"')}");\n`;
+            }
         }
     }
 
     while (match = reg.exec(tpl)) {
         add(tpl.slice(cursor, match.index));
-        add(`this.${match[1]}`, true);
+        add(match[1], true);
         cursor = match.index + match[0].length;
     }
     add(tpl.substr(cursor, tpl.length - cursor));
--- code-training/template/test.js
+++ code-training/template/test.js
@@ -2,16 +2,53 @@ import {simpleTpl} from "./index";
 
 test('simpleTpl', () => {
 
-    let tpl = '我是<%=foo%>,他是<%=bar%>.';
+    let tpl = '我是<%=this.foo%>,他是<%=this.bar%>.' +
+        '<%if(this.arr){%>' +
+        '<% for(var i=0;i<this.arr.length;i++){%>' +
+        '<a href="#"><%=this.arr[i]%></a>' +
+        '<%}%>' +
+        '<%} else {%>' +
+        '<p>aa</p>' +
+        '<%}%>';
     let data = {
         foo: 'foo',
         bar: 'bar',
+        arr: ['haha', 'xixi'],
     }
     let template = simpleTpl(tpl, data);
 
     console.log(template);
 
 })
+
+
+test('tpl', () => {
+    function foo() {
+        var r = [];
+        r.push("我是");
+        r.push(this.foo);
+        r.push(",他是");
+        r.push(this.bar);
+        r.push(".");
+        if (this.arr) {
+            for (var i = 0; i < this.arr.length; i++) {
+                r.push("<a href=\"#\">");
+                r.push(this.arr[i]);
+                r.push("</a>");
+            }
+        } else {
+            r.push("<p>aa</p>");
+        }
+        return r.join("");
+    }
+
+    let data = {
+        foo: 'foo',
+        bar: 'bar',
+        arr: ['haha', 'xixi'],
+    }
+    console.log(foo.call(data));
+});
 
 test('test', () => {
     let data = {
